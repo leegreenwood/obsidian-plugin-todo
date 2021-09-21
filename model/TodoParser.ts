61897515de7b333493490c3fc26e3cc1d7876e50
Lee Greenwood <leegreenwood@users.noreply.github.com>
2021-09-21 12:11:27 +0100

    Modified to support Logseq TODO Workflow

--- model/TodoParser.ts
+++ model/TodoParser.ts
@@ -2,13 +2,13 @@ import { TodoItem, TodoItemStatus } from '../model/TodoItem';
 
 export class TodoParser {
   async parseTasks(filePath: string, fileContents: string): Promise<TodoItem[]> {
-    const pattern = /(-|\*) \[(\s|x)?\]\s(.*)/g;
+    const pattern = /(-) (TODO|DOING|DONE)+\s(.*)/g;
     return [...fileContents.matchAll(pattern)].map((task) => this.parseTask(filePath, task));
   }
 
   private parseTask(filePath: string, entry: RegExpMatchArray): TodoItem {
     const todoItemOffset = 2; // Strip off `-|* `
-    const status = entry[2] === 'x' ? TodoItemStatus.Done : TodoItemStatus.Todo;
+    const status = entry[2] === 'DONE' ? TodoItemStatus.Done : TodoItemStatus.Todo;
     const description = entry[3];
 
     const datePattern = /#(\d{4}-\d{2}-\d{2})/g;
